1c427fb277cbe4026d91b359e67764d861ece201
akurochka <andrii.kurochka@zorallabs.com>
2018-03-06 08:43:09 +0200

    Creates initial solution structure

---
+++ src/ComparerService.App/Controllers/DiffController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComparerService.App.Controllers
+{
+    [Route("v1/[controller]")]
+    public class DiffController : Controller
+    {
+        [HttpPost]
+        [Route("{id}/left")]
+        public Task<IActionResult> SetLeft([FromRoute] string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("{id}/right")]
+        public Task<IActionResult> SetRight([FromRoute] string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Task<IActionResult> GetDiff([FromRoute] string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Comparer.Tests/DiffServiceTests.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Comparer.Tests
+{
+    public class DiffServiceTests
+    {
+    }
+}
